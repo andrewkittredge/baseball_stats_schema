61d78b384687ab5d57b21862e5d63e23a7c4fea9
andrew_kittredge <andrewlkittredge@gmail.com>
2011-06-27 00:53:20 -0400

    More changes.

--- create_baseball_schema.py
+++ create_baseball_schema.py
@@ -7,6 +7,9 @@ from baseball_stats_models import YearlyBattingStats, Team, Player
 
 import sql_alchemy_utils
 
+#from Source.Baseball_Reference_Scraping.baseball_reference import get_all_player_stats
+import Source.Baseball_Reference_Scraping.baseball_reference
+
 def write_player_and_stats(session, player, stats):
 
     player = Player(player)
@@ -22,7 +25,11 @@ def write_player_and_stats(session, player, stats):
 def main():
     global db_password
     db_password = sys.argv[1]
-    initialize_schema()
+
+    session = sql_alchemy_utils.get_session(db_password)
+
+    for player, stats in get_all_players_stats():
+        write_player_and_stats(session, player, stats)
 
     return 0
 
